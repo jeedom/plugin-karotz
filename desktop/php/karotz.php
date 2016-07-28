dc5abfa662094b989f1f43df8c1835a733061e22
Loïc <loic@jeedom.com>
2016-07-28 18:16:15 +0200

    Improve UI

--- desktop/php/karotz.php
+++ desktop/php/karotz.php
@@ -27,12 +27,12 @@ foreach ($eqLogics as $eqLogic) {
     <legend>{{Mes Karotzs}}</legend>
     <div class="eqLogicThumbnailContainer">
       <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-         <center>
-            <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
-        </center>
-        <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;;color:#94ca02"><center>Ajouter</center></span>
-    </div>
-    <?php
+       <center>
+        <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+    </center>
+    <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;;color:#94ca02"><center>Ajouter</center></span>
+</div>
+<?php
 foreach ($eqLogics as $eqLogic) {
 	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
@@ -46,100 +46,106 @@ foreach ($eqLogics as $eqLogic) {
 </div>
 </div>
 <div class="col-lg-10 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-    <form class="form-horizontal">
-        <fieldset>
-            <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-            <div class="form-group">
-                <label class="col-lg-2 control-label">{{Nom de l'équipement}}</label>
-                <div class="col-lg-2">
-                    <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}"/>
+
+   <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+   <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+
+   <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+</ul>
+
+<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+    <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+        <form class="form-horizontal">
+            <fieldset>
+                <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+                <div class="form-group">
+                    <label class="col-lg-2 control-label">{{Nom de l'équipement}}</label>
+                    <div class="col-lg-2">
+                        <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                        <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}"/>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label class="col-lg-2 control-label" >{{Objet parent}}</label>
-                <div class="col-lg-2">
-                    <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-                        <option value="">{{Aucun}}</option>
-                        <?php
+                <div class="form-group">
+                    <label class="col-lg-2 control-label" >{{Objet parent}}</label>
+                    <div class="col-lg-2">
+                        <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                            <option value="">{{Aucun}}</option>
+                            <?php
 foreach (object::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
-                   </select>
+                       </select>
+                   </div>
                </div>
-           </div>
-           <div class="form-group">
-            <label class="col-lg-2 control-label">{{Catégorie}}</label>
-            <div class="col-lg-10">
-                <?php
+               <div class="form-group">
+                <label class="col-lg-2 control-label">{{Catégorie}}</label>
+                <div class="col-lg-10">
+                    <?php
 foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
 	echo '<label class="checkbox-inline">';
 	echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
 	echo '</label>';
 }
 ?>
+               </div>
            </div>
-       </div>
-       <div class="form-group">
-        <label class="col-sm-2 control-label"></label>
-        <div class="col-sm-9">
-            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+           <div class="form-group">
+            <label class="col-sm-2 control-label"></label>
+            <div class="col-sm-9">
+                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label class="col-lg-2 control-label">{{Adresse IP}}</label>
-        <div class="col-lg-2">
-            <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="addr" placeholder="{{Adresse IP}}"/>
+        <div class="form-group">
+            <label class="col-lg-2 control-label">{{Adresse IP}}</label>
+            <div class="col-lg-2">
+                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="addr" placeholder="{{Adresse IP}}"/>
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label class="col-lg-2 control-label">{{Moteur de TTS}}</label>
-        <div class="col-lg-2">
-            <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="ttsengine" placeholder="{{Moteur TTS}}"/>
+        <div class="form-group">
+            <label class="col-lg-2 control-label">{{Moteur de TTS}}</label>
+            <div class="col-lg-2">
+                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="ttsengine" placeholder="{{Moteur TTS}}"/>
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label class="col-lg-2 control-label">{{Activer squeezebox}}</label>
-        <div class="col-lg-2">
-            <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="enablesqueezebox"/>
+        <div class="form-group">
+            <label class="col-lg-2 control-label">{{Activer squeezebox}}</label>
+            <div class="col-lg-2">
+                <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="enablesqueezebox"/>
+            </div>
         </div>
-    </div>
-    <div class="form-group">
-        <label class="col-lg-2 control-label">{{Activer clock}}</label>
-        <div class="col-lg-2">
-            <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="enableclock"/>
+        <div class="form-group">
+            <label class="col-lg-2 control-label">{{Activer clock}}</label>
+            <div class="col-lg-2">
+                <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="enableclock"/>
+            </div>
         </div>
-    </div>
-     <div class="form-group">
-        <label class="col-lg-2 control-label">{{Activer moods}}</label>
-        <div class="col-lg-2">
-            <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="enablemoods"/>
-        </div>
-    </div>
-</fieldset>
-</form>
-
-<legend>Commandes</legend>
-<table id="table_cmd" class="table table-bordered table-condensed">
-    <thead>
-        <tr>
-            <th>{{Nom}}</th>
-            <th>{{Action}}</th>
-        </tr>
-    </thead>
-    <tbody></tbody>
-</table>
-
-<form class="form-horizontal">
-    <fieldset>
-        <div class="form-actions">
-            <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-            <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+        <div class="form-group">
+            <label class="col-lg-2 control-label">{{Activer moods}}</label>
+            <div class="col-lg-2">
+                <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="enablemoods"/>
+            </div>
         </div>
     </fieldset>
 </form>
+
+</div>
+<div role="tabpanel" class="tab-pane" id="commandtab">
+    <table id="table_cmd" class="table table-bordered table-condensed">
+        <thead>
+            <tr>
+                <th>{{Nom}}</th>
+                <th>{{Action}}</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+
+</div>
+</div>
 
 </div>
 </div>
